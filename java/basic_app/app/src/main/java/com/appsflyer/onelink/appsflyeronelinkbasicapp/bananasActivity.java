a71b718d934b92a0f9a1a935b97f1c9d3fa4900b
liaz.kamper <liaz.kamper@appsflyer.com>
2020-11-23 10:49:49 +0200

    Fix deep linking to Peaches and Bananas

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/bananasActivity.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/bananasActivity.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.appsflyer.deeplink.DeepLink;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 import static com.appsflyer.onelink.appsflyeronelinkbasicapp.AppsflyerBasicApp.LOG_TAG;
@@ -18,15 +24,16 @@ public class bananasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bananas);
 
         Intent intent = getIntent();
-        HashMap<String, String> dlAttrMap = (HashMap<String, String>) intent.getSerializableExtra(AppsflyerBasicApp.DL_ATTRS);
-        if (dlAttrMap != null) {
+        DeepLink dlData= new Gson().fromJson(intent.getStringExtra(AppsflyerBasicApp.DL_ATTRS),DeepLink.class);
+        if (dlData != null) {
             TextView dlAttrsText = findViewById(R.id.bananas_deeplinkparams);
-            String dlAttrStrings = "";
-            for (String attrName : dlAttrMap.keySet()) {
-                String deepLinkAttrStr = attrName + " = " + dlAttrMap.get(attrName) + "\n";
-                dlAttrStrings += deepLinkAttrStr;
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(dlData.toString());
+                dlAttrsText.setText(jsonObject.toString(4).replaceAll("\\\\",""));// 4 is no of spaces for indent
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            dlAttrsText.setText(dlAttrStrings);
         } else {
             TextView dlTitleText = findViewById(R.id.bananas_deeplinktitle);
             dlTitleText.setText("No Deep Linking Happened");
