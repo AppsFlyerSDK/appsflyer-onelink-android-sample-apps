9b663f43f33faada58fc422e32f39ca01027efa5
Liaz Kamper <liaz.kamper@appsflyer.com>
2022-05-15 16:07:07 +0300

    Step 3 - Conversion data

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
@@ -31,38 +31,84 @@ public class AppsflyerBasicApp extends Application {
         appsflyer.setDebugLog(true);
         appsflyer.setMinTimeBetweenSessions(0);
 
-        appsflyer.init(afDevKey, null, this);
+        AppsFlyerConversionListener conversionListener =  new AppsFlyerConversionListener() {
+            @Override
+            public void onConversionDataSuccess(Map<String, Object> conversionDataMap) {
+                for (String attrName : conversionDataMap.keySet())
+                    Log.d(LOG_TAG, "Conversion attribute: " + attrName + " = " + conversionDataMap.get(attrName));
+                String status = Objects.requireNonNull(conversionDataMap.get("af_status")).toString();
+                if(status.equals("Non-organic")){
+                    if( Objects.requireNonNull(conversionDataMap.get("is_first_launch")).toString().equals("true")){
+                        Log.d(LOG_TAG,"Conversion: First Launch");
+                        //Deferred deep link in case of a legacy link
+                        if(conversionDataMap.containsKey("fruit_name")){
+                            if (conversionDataMap.containsKey("deep_link_value")) { //Not legacy link
+                                Log.d(LOG_TAG,"onConversionDataSuccess: Link contains deep_link_value, deep linking with UDL");
+                            }
+                            else{ //Legacy link
+                                conversionDataMap.put("deep_link_value", conversionDataMap.get("fruit_name"));
+                                String fruitNameStr = (String) conversionDataMap.get("fruit_name");
+                                DeepLink deepLinkData = mapToDeepLinkObject(conversionDataMap);
+                                goToFruit(fruitNameStr, deepLinkData);
+                            }
+                        }
+                    } else {
+                        Log.d(LOG_TAG,"Conversion: Not First Launch");
+                    }
+                } else {
+                    Log.d(LOG_TAG, "Conversion: This is an organic install.");
+                }
+                conversionData = conversionDataMap;
+            }
+
+            @Override
+            public void onConversionDataFail(String errorMessage) {
+                Log.d(LOG_TAG, "error getting conversion data: " + errorMessage);
+            }
+
+            @Override
+            public void onAppOpenAttribution(Map<String, String> attributionData) {
+                Log.d(LOG_TAG, "onAppOpenAttribution: This is fake call.");
+            }
+
+            @Override
+            public void onAttributionFailure(String errorMessage) {
+                Log.d(LOG_TAG, "error onAttributionFailure : " + errorMessage);
+            }
+        };
+
+        appsflyer.init(afDevKey, conversionListener, this);
         appsflyer.start(this);
     }
 
-//    private void goToFruit(String fruitName, DeepLink dlData) {
-//        String fruitClassName = (fruitName.substring(0, 1).toUpperCase() + fruitName.substring(1)).concat("Activity");
-//        try {
-//            Class fruitClass = Class.forName(this.getPackageName().concat(".").concat(fruitClassName));
-//            Log.d(LOG_TAG, "Looking for class " + fruitClass);
-//            Intent intent = new Intent(getApplicationContext(), fruitClass);
-//            if (dlData != null) {
-//                // TODO - make DeepLink Parcelable
-//                String objToStr = new Gson().toJson(dlData);
-//                intent.putExtra(DL_ATTRS, objToStr);
-//            }
-//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            startActivity(intent);
-//        } catch (ClassNotFoundException e) {
-//            Log.d(LOG_TAG, "Deep linking failed looking for " + fruitName);
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public DeepLink mapToDeepLinkObject(Map <String, Object> conversionDataMap){
-//        try {
-//            String objToStr = new Gson().toJson(conversionDataMap);
-//            DeepLink deepLink = DeepLink.AFKeystoreWrapper(new JSONObject(objToStr));
-//            return deepLink;
-//        }
-//        catch (org.json.JSONException e ){
-//            Log.d(LOG_TAG, "Error when converting map to DeepLink object: " + e.toString());
-//        }
-//        return null;
-//    }
+    private void goToFruit(String fruitName, DeepLink dlData) {
+        String fruitClassName = (fruitName.substring(0, 1).toUpperCase() + fruitName.substring(1)).concat("Activity");
+        try {
+            Class fruitClass = Class.forName(this.getPackageName().concat(".").concat(fruitClassName));
+            Log.d(LOG_TAG, "Looking for class " + fruitClass);
+            Intent intent = new Intent(getApplicationContext(), fruitClass);
+            if (dlData != null) {
+                // TODO - make DeepLink Parcelable
+                String objToStr = new Gson().toJson(dlData);
+                intent.putExtra(DL_ATTRS, objToStr);
+            }
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } catch (ClassNotFoundException e) {
+            Log.d(LOG_TAG, "Deep linking failed looking for " + fruitName);
+            e.printStackTrace();
+        }
+    }
+
+    public DeepLink mapToDeepLinkObject(Map <String, Object> conversionDataMap){
+        try {
+            String objToStr = new Gson().toJson(conversionDataMap);
+            DeepLink deepLink = DeepLink.AFKeystoreWrapper(new JSONObject(objToStr));
+            return deepLink;
+        }
+        catch (org.json.JSONException e ){
+            Log.d(LOG_TAG, "Error when converting map to DeepLink object: " + e.toString());
+        }
+        return null;
+    }
 }
