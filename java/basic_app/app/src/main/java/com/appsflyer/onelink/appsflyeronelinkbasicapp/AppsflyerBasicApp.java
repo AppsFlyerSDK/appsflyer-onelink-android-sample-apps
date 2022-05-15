4fe942df8a2c5518d149bba53e33e438cd36dbd2
Liaz Kamper <liaz.kamper@appsflyer.com>
2022-05-15 15:26:59 +0300

    First step - no SDK

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
@@ -20,4 +20,35 @@ public class AppsflyerBasicApp extends Application {
     public void onCreate() {
         super.onCreate();
     }
+
+//    private void goToFruit(String fruitName, DeepLink dlData) {
+//        String fruitClassName = (fruitName.substring(0, 1).toUpperCase() + fruitName.substring(1)).concat("Activity");
+//        try {
+//            Class fruitClass = Class.forName(this.getPackageName().concat(".").concat(fruitClassName));
+//            Log.d(LOG_TAG, "Looking for class " + fruitClass);
+//            Intent intent = new Intent(getApplicationContext(), fruitClass);
+//            if (dlData != null) {
+//                // TODO - make DeepLink Parcelable
+//                String objToStr = new Gson().toJson(dlData);
+//                intent.putExtra(DL_ATTRS, objToStr);
+//            }
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(intent);
+//        } catch (ClassNotFoundException e) {
+//            Log.d(LOG_TAG, "Deep linking failed looking for " + fruitName);
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public DeepLink mapToDeepLinkObject(Map <String, Object> conversionDataMap){
+//        try {
+//            String objToStr = new Gson().toJson(conversionDataMap);
+//            DeepLink deepLink = DeepLink.AFKeystoreWrapper(new JSONObject(objToStr));
+//            return deepLink;
+//        }
+//        catch (org.json.JSONException e ){
+//            Log.d(LOG_TAG, "Error when converting map to DeepLink object: " + e.toString());
+//        }
+//        return null;
+//    }
 }
