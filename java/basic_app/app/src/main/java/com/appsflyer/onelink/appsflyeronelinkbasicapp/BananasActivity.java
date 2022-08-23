ee45bad01ce9ce9989caa14754d433b0e76748ac
Liaz Kamper <liaz.kamper@appsflyer.com>
2022-08-23 23:36:12 +0300

    Step 5 - TODO Add events

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/BananasActivity.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/BananasActivity.java
@@ -1,6 +1,19 @@
 package com.appsflyer.onelink.appsflyeronelinkbasicapp;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+import androidx.annotation.NonNull;
+
+import com.appsflyer.AppsFlyerLib;
+import com.appsflyer.AFInAppEventType; // Predefined event names
+import com.appsflyer.AFInAppEventParameterName; // Predefined parameter names
+import com.appsflyer.attribution.AppsFlyerRequestListener;
+
+import static com.appsflyer.onelink.appsflyeronelinkbasicapp.AppsflyerBasicApp.LOG_TAG;
+
 
 public class BananasActivity extends FruitActivity {
 
@@ -10,10 +23,15 @@ public class BananasActivity extends FruitActivity {
         setStaticAttributes("bananas");
         showFruitAmount();
         showDlData();
+        sendEventAsync();
     }
 
     @Override
     protected int getLayoutResourceId() {
         return R.layout.activity_bananas;
+    }
+
+    protected void sendEventAsync() {
+       // Add event with Async wait here
     }
 }
--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/PeachesActivity.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/PeachesActivity.java
@@ -2,6 +2,13 @@ package com.appsflyer.onelink.appsflyeronelinkbasicapp;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.appsflyer.AppsFlyerLib;
+import com.appsflyer.AFInAppEventType; // Predefined event names
+import com.appsflyer.AFInAppEventParameterName; // Predefined parameter names
+
 public class PeachesActivity extends FruitActivity {
 
     @Override
@@ -10,10 +17,16 @@ public class PeachesActivity extends FruitActivity {
         setStaticAttributes("peaches");
         showFruitAmount();
         showDlData();
+        sendEvent();
     }
 
     @Override
     protected int getLayoutResourceId() {
         return R.layout.activity_peaches;
     }
+
+    protected void sendEvent() {
+        //Add event here
+    }
+
 }
