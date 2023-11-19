87dc399ed77fb3dea2133d6eb2ff1473a8ab205b
GilDev23 <nehoraygil17@gmail.com>
2023-11-19 14:17:37 +0200

    .

--- kotlin/basic_app/build.gradle.kts
+++ kotlin/basic_app/build.gradle.kts
@@ -3,8 +3,3 @@ plugins {
     id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
-// ...
-repositories {
-    mavenCentral()
-}
-/// ...
