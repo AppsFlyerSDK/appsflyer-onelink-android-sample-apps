322de027994ff2cb09a3c1362138f0799e70bf94
GilDev23 <nehoraygil17@gmail.com>
2023-11-19 14:08:59 +0200

    Added FruitActivity and integrate SDK

--- kotlin/basic_app/app/build.gradle.kts
+++ kotlin/basic_app/app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.appsflyer:af-android-sdk:HERE_LATEST_VERSION")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
---
+++ kotlin/basic_app/app/src/main/java/com/example/onelinkbasicapp/FruitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onelinkbasicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FruitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fruit)
+    }
+}
