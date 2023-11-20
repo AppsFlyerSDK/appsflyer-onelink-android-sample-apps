7377220bc6ac8830d2f813daf06d834e9082fecd
RefefFR <surafelshif@gmail.com>
2023-11-20 14:18:36 +0200

    Added Comments

--- kotlin/basic_app/app/src/main/java/com/example/onelinkbasicapp/PeachesActivity.kt
+++ kotlin/basic_app/app/src/main/java/com/example/onelinkbasicapp/PeachesActivity.kt
@@ -7,7 +7,7 @@ class PeachesActivity : FruitActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    //מחזיר אל ה-fruitactivity את האידי של אותו מחלקה כדי שיוכל לשנות את התצוגה שלו דרך הfruitactivity
+    //מחזיר אל ה-fruitactivity את האידי של אותו מחלקה כדי שיוכל לשנות את התצוגה שלו דרך הfruit-activity
     override fun getLayoutResourceId(): Int {
         return R.layout.activity_peaches
     }
