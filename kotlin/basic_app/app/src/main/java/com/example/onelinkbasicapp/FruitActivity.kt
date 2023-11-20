c9e6345d782578b90c43ed62f17973ae23012c66
GilDev23 <nehoraygil17@gmail.com>
2023-11-20 10:01:30 +0200

    Added fruit activity

--- kotlin/basic_app/app/src/main/java/com/example/onelinkbasicapp/FruitActivity.kt
+++ kotlin/basic_app/app/src/main/java/com/example/onelinkbasicapp/FruitActivity.kt
@@ -1,11 +1,20 @@
 package com.example.onelinkbasicapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class FruitActivity : AppCompatActivity() {
+
+public abstract class FruitActivity : AppCompatActivity() {
+    var dlAttrs: TextView? = null
+    var dlTitleText: TextView? = null
+    var goToConversionDataText: TextView? = null
+    var fruitName: String? = null
+    var fruitAmountStr: String? = null
+    var fruitAmount: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fruit)
     }
 }
