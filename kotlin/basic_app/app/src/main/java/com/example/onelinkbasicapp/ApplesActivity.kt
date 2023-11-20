5fba10cdb2ba765e98c407a7277d1bd55696d9da
RefefFR <surafelshif@gmail.com>
2023-11-20 13:45:59 +0200

    Added Fruit Activity+Permission from each activity to its xml

--- kotlin/basic_app/app/src/main/java/com/example/onelinkbasicapp/ApplesActivity.kt
+++ kotlin/basic_app/app/src/main/java/com/example/onelinkbasicapp/ApplesActivity.kt
@@ -3,9 +3,13 @@ package com.example.onelinkbasicapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ApplesActivity : AppCompatActivity() {
+class ApplesActivity : FruitActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apples)
+    }
+    //
+    override protected fun getLayoutResourceId():Int{
+        return R.layout.activity_apples;
     }
 }
