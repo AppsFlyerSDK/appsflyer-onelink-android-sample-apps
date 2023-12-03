bf7dede8cec89d1017bd693ed3675580c89a9df9
AFROJOKER <maximgav123@gmail.com>
2023-12-03 13:17:21 +0200

    Upload constant values and images

---
+++ kotlin/basic_app/app/src/main/kotlin/com/appsflyer/onelink/appsflyeronelinkbasicapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.appsflyer.onelink.appsflyeronelinkbasicapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       setContentView(R.layout.activity_main)
+    }
+}
+
