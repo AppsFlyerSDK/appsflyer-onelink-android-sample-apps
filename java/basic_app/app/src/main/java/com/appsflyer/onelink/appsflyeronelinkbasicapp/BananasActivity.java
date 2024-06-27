4452b35ddf53ac3d0e97489ded806d61e8b593f3
Liaz Kamper <liaz.kamper@appsflyer.com>
2024-06-27 14:06:54 +0300

    Step 1 - Empty app

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/BananasActivity.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/BananasActivity.java
@@ -1,6 +1,19 @@
 package com.appsflyer.onelink.appsflyeronelinkbasicapp;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+import androidx.annotation.NonNull;
+
+import com.appsflyer.AppsFlyerLib;
+import com.appsflyer.AFInAppEventType; // Predefined event names
+import com.appsflyer.AFInAppEventParameterName; // Predefined parameter names
+import com.appsflyer.attribution.AppsFlyerRequestListener;
+
+import static com.appsflyer.onelink.appsflyeronelinkbasicapp.AppsflyerBasicApp.LOG_TAG;
+
 
 public class BananasActivity extends FruitActivity {
 
@@ -10,10 +23,35 @@ public class BananasActivity extends FruitActivity {
         setStaticAttributes("bananas");
         showFruitAmount();
         showDlData();
+        sendEventAsync();
     }
 
     @Override
     protected int getLayoutResourceId() {
         return R.layout.activity_bananas;
+    }
+
+    protected void sendEventAsync() {
+        Map<String, Object> eventValues = new HashMap<String, Object>();
+        eventValues.put(AFInAppEventParameterName.PRICE, 1234.56);
+        eventValues.put(AFInAppEventParameterName.CURRENCY, "USD");
+        eventValues.put(AFInAppEventParameterName.CONTENT_ID, this.fruitName);
+        eventValues.put(AFInAppEventParameterName.CONTENT_TYPE, "fruits");
+
+        AppsFlyerLib.getInstance().logEvent(getApplicationContext(),
+                AFInAppEventType.CONTENT_VIEW,
+                eventValues,
+                new AppsFlyerRequestListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d(LOG_TAG, "Event sent successfully");
+                    }
+                    @Override
+                    public void onError(int i, @NonNull String s) {
+                        Log.d(LOG_TAG, "Event failed to be sent:\n" +
+                                "Error code: " + i + "\n"
+                                + "Error description: " + s);
+                    }
+                });
     }
 }
--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/PeachesActivity.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/PeachesActivity.java
@@ -2,6 +2,13 @@ package com.appsflyer.onelink.appsflyeronelinkbasicapp;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.appsflyer.AppsFlyerLib;
+import com.appsflyer.AFInAppEventType; // Predefined event names
+import com.appsflyer.AFInAppEventParameterName; // Predefined parameter names
+
 public class PeachesActivity extends FruitActivity {
 
     @Override
@@ -10,10 +17,22 @@ public class PeachesActivity extends FruitActivity {
         setStaticAttributes("peaches");
         showFruitAmount();
         showDlData();
+        sendEvent();
     }
 
     @Override
     protected int getLayoutResourceId() {
         return R.layout.activity_peaches;
     }
+
+    protected void sendEvent() {
+        Map<String, Object> eventValues = new HashMap<String, Object>();
+        eventValues.put(AFInAppEventParameterName.PRICE, 1234.56);
+        eventValues.put(AFInAppEventParameterName.CURRENCY, "USD");
+        eventValues.put(AFInAppEventParameterName.CONTENT_ID, this.fruitName);
+
+        AppsFlyerLib.getInstance().logEvent(getApplicationContext(),
+                AFInAppEventType.ADD_TO_WISH_LIST , eventValues);
+    }
+
 }
