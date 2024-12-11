6550d633aca5c7e3c08ed2ed5215c803b64cccd7
Liaz Kamper <liaz.kamper@appsflyer.com>
2024-12-11 21:55:03 +0200

    Remove some Branch params params

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/MainActivity.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/MainActivity.java
@@ -93,12 +93,8 @@ public class MainActivity extends AppCompatActivity {
                 .setCanonicalUrl("https://test_canonical_url")
                 .setContentMetadata(
                         new ContentMetadata()
-                                .setAddress("Street_Name", "test city", "test_state", "test_country", "test_postal_code")
-                                .setLocation(-151.67, -124.0)
                                 .setPrice(10.0, CurrencyType.USD)
-                                .setQuantity(1.5)
-                                .setSku("test_sku")
-                                .setContentSchema(BranchContentSchema.COMMERCE_PRODUCT));
+                                .setQuantity(1.5));
 
 //  Create an event
         new BranchEvent(BRANCH_STANDARD_EVENT.ADD_TO_CART)
