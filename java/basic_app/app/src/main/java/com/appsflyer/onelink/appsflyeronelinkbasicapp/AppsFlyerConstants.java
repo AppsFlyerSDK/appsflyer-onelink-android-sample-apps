cc49bb8b12f0908d773ef9a0ff624fa8ce0e9885
Liaz Kamper <liaz.kamper@appsflyer.com>
2025-06-29 10:39:37 +0300

    Empty app set for SDK 33

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsFlyerConstants.java
+++
@@ -1,7 +0,0 @@
-package com.appsflyer.onelink.appsflyeronelinkbasicapp;
-
-
-public class AppsFlyerConstants {
-    //This is the dev key of AppsFlyer's demo account and MUST be changed to your app's dev key
-    static final String afDevKey = "sQ84wpdxRTR4RMCaE9YqS4";
-}
